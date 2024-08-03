5488a8a5e916103f220604bd4a39279a4e263064
Daniel McCormack <daniel.mccormack@sas.com>
2024-08-03 14:39:12 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,94 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
+
+interface teams {
+    premTeam: string
+      premOdds: string
+      championshipTeam: string
+      championshipOdds: string
+      league1Team: string
+      league1Odds: string
+      league2Team: string
+      league2odds: string
+      lowlandTeam: string
+      lowlandOdds: string
+      eplTeam: string
+      eplOdds: string
+      championsLeagueTeam: string,
+      championsLeagueOdds: string
+
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'football-predictions';
+  
+  gordonTeams: teams =  {
+    premTeam: "Celtic",
+    premOdds: "",
+    championshipTeam: "Partick Thistle",
+    championshipOdds: "",
+    league1Team: "Alloa",
+    league1Odds: "",
+    league2Team: "Spartans",
+    league2odds: "",
+    lowlandTeam: "East Kilbride",
+    lowlandOdds: "",
+    eplTeam: "Man City",
+    eplOdds: "",
+    championsLeagueTeam: "Man City",
+    championsLeagueOdds: ""
+  }
+  
+  lyleTeams: teams = {
+    premTeam: "Celtic",
+    premOdds: "",
+    championshipTeam: "Partick Thistle",
+    championshipOdds: "",
+    league1Team: "Alloa",
+    league1Odds: "",
+    league2Team: "Clyde",
+    league2odds: "",
+    lowlandTeam: "Albion Rovers",
+    lowlandOdds: "",
+    eplTeam: "Arsenal",
+    eplOdds: "",
+    championsLeagueTeam: "Bayern Munich",
+    championsLeagueOdds: ""
+  }
+
+  danTeams: teams = {
+    premTeam: "Celtic",
+    premOdds: "",
+    championshipTeam: "Livingston",
+    championshipOdds: "",
+    league1Team: "Inverness",
+    league1Odds: "",
+    league2Team: "Forfar",
+    league2odds: "",
+    lowlandTeam: "Linlithgow Rose",
+    lowlandOdds: "",
+    eplTeam: "Man City",
+    eplOdds: "",
+    championsLeagueTeam: "Bayern Munich",
+    championsLeagueOdds: ""
+  }
+ 
+  loadingSpin = true;
+
+  ngOnInit() {
+    this.loadingSpin = true;
+    setTimeout(() => {
+      this.loadingSpin = false;
+    }, 3000)
+  }
+
 }
